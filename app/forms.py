4130c4cfee834bbd6a7f682de1969f1b8b69dbcb
ameyrajmane <ameyrajmane8@gmail.com>
2022-01-02 09:33:04 +0530

    changes done

--- app/forms.py
+++ app/forms.py
@@ -10,8 +10,7 @@ from .models import Customer
 
 class CustomerRegistrationForm(UserCreationForm):
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(label='Confirm Password (again)',
-                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     email = forms.CharField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control'}))
 
 
